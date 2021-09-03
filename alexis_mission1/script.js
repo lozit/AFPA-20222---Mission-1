338d31fe8ebaeea1e2bbf20a5c2cfa3f1e7d910b
Bultel <bultelalexis@gmail.com>
2021-09-03 11:58:00 +0200

    v1.1

--- alexis_mission1/script.js
+++ alexis_mission1/script.js
@@ -1,23 +1,21 @@
 let saisieClavier;
 let nombreDesaisie = [];
 let moyenne = 0;
+
+const addition = (nombreA, nombreB) => nombreA + nombreB; //fonction addition du tableau
+
 do{
    saisieClavier = Number (prompt ("Entrer un nombre"));
    nombreDesaisie.push (saisieClavier);
 }
 while (saisieClavier != 0) {
-    console.log (nombreDesaisie.length-1);
-    function getMaxTableau(nombreDesaisie) {
-      return Math.max.apply(null, nombreDesaisie);
-  }
+    // nombreDesaisie = parseInt (nombreDesaisie);
+    moyenne = nombreDesaisie.reduce(addition)/(nombreDesaisie.length-1);
 };
+
+
+console.log (nombreDesaisie.length-1);
+console.log (Math.min (nombreDesaisie));
+console.log(nombreDesaisie.reduce(addition));
+console.log (moyenne);
 console.log (nombreDesaisie);
-
-
-
-// const arr = [1, 2, 3, 4];
-// const reducer = (accumulator, curr) => accumulator + curr;
-// console.log(arr.reduce(reducer));
-
-
-// console.log (nombreDesaisie);
