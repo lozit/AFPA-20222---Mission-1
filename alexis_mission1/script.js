4899b224a96981080445db9a2647e7f46499f9e1
Bultel <bultelalexis@gmail.com>
2021-09-14 08:13:40 +0200

    V2

--- alexis_mission1/script.js
+++ alexis_mission1/script.js
@@ -1,21 +1,42 @@
+// Declaration de mes variables
 let saisieClavier;
 let nombreDesaisie = [];
-let moyenne = 0;
+let valeurMax = [];
+let valeurMin = [];
+let somme;
+let i = 0;
+let longueurTab;
 
-const addition = (nombreA, nombreB) => nombreA + nombreB; //fonction addition du tableau
+//Declaration de mes fonctions
 
+  //addition
+addition = (previousValue, currentValue) => previousValue + currentValue;   //fonction fléchée addition
+
+  //moyenne
+function moyenne(array) {
+  for (let i = 0; i < array.length; i++) {
+    valeurMoyenne = array.reduce(addition) / array.length;     //reduce liste tout les elements du array
+    return valeurMoyenne;
+  };
+};
+  //boucle pour recuperer les saisies de l'utilisateur
 do{
-   saisieClavier = Number (prompt ("Entrer un nombre"));
-   nombreDesaisie.push (saisieClavier);
+   saisieClavier = Number (prompt ("Entrez vos données \n0 pour terminer la saisie"));    //recupere les saisies de l'utilisateur
+   nombreDesaisie.push (saisieClavier);                       //rempli mon array
 }
 while (saisieClavier != 0) {
-    // nombreDesaisie = parseInt (nombreDesaisie);
-    moyenne = nombreDesaisie.reduce(addition)/(nombreDesaisie.length-1);
+    nombreDesaisie.pop();                                    //supprime la valeur 0 de fin de boucle
+    longueurTab = nombreDesaisie.length;                     //recupere la longueur de mon array 
+    valeurMax = Math.max(...nombreDesaisie);                //fonction JS valeur max
+    valeurMin = Math.min(...nombreDesaisie);                //fonction JS valeur min
 };
 
+somme = nombreDesaisie.reduce(addition);                    //affectation de mon resultat addition dans somme                  
 
-console.log (nombreDesaisie.length-1);
-console.log (Math.min (nombreDesaisie));
-console.log(nombreDesaisie.reduce(addition));
-console.log (moyenne);
-console.log (nombreDesaisie);
+// console.log (somme);
+// console.log ("somme "+ somme);
+// console.log (valeurMax);                                  // test dans ma console
+// console.log (valeurMin);
+// moyenne(nombreDesaisie);
+
+alert (longueurTab+" Nombres saisie\n" + "Compris entre "+valeurMin+" et "+valeurMax + "\nPour une somme de "+ somme +"\nEt pour une moyenne de "+ moyenne(nombreDesaisie));
